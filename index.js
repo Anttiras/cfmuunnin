71ce6af3eb13cd52b621652f193e8293ccef590b
Anttiras <antti.rasanen@seamk.fi>
2021-09-10 16:11:22 +0300

    asdasd

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-function muunna() {
-    alert('Täällä ollaan');
+const fahrenheitSyöte = document.querySelector('input');
+const fahrenheitTuloste = document.querySelector('#fahrenheit');
+const CelciusTuloste = document.querySelector('#celcius');
+const muunnaButton = document.querySelector('button');
+
+function fahrenheitCelciukseksi(fahrenheitSyote) {
+    let celsius = fahrenheit;
+    return celcius;
 }
 
-const button = document.querySelector('button');
-button.addEventListener('click', muunna);
+function muunna() {
+    let syote = fahrenheitSyöte.value;
+    fahrenheitTuloste.textContent = syote;
+
+    CelciusTuloste.textContent = fahrenheitCelciukseksi(syote);
+}
+
+muunnaButton.addEventListener('click', muunna);
